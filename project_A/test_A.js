427b8bc944e28fe4c1e55805458af24182c3a08a
Yuchen-cyber <wangyuchen0709@126.com>
2024-01-13 09:41:20 +0000

    finish the lemon 3d model

--- project_A/test_A.js
+++ project_A/test_A.js
@@ -89,12 +89,12 @@ function initVertexBuffer(gl) {
 // shapes.
 	
 		// Make each 3D shape in its own array of vertices:
-	makeCylinder();					// create, fill the cylVerts array
+		makeEgg();					// create, fill the eggVerts array
 	// makeGroundGrid();				// create, fill the gndVerts array
 	makeDiamond(); //create, fill the diaVerts array
 	// how many floats total needed to store all shapes?
-	// var mySiz = (cylVerts.length + diaVerts.length + gndVerts.length);		
-	var mySiz = (cylVerts.length + diaVerts.length);						
+	// var mySiz = (eggVerts.length + diaVerts.length + gndVerts.length);		
+	var mySiz = (eggVerts.length + diaVerts.length);						
 
 	// How many vertices total?
 	var nn = mySiz / floatsPerVertex;
@@ -102,13 +102,12 @@ function initVertexBuffer(gl) {
 	// Copy all shapes into one big Float32 array:
 	var colorShapes = new Float32Array(mySiz);
 	// Copy them:  remember where to start for each shape:
-	cylStart = 0;							// we stored the cylinder first.
-	for(i=0,j=0; j< cylVerts.length; i++,j++) {
-		colorShapes[i] = cylVerts[j];
+	eggSatrt = 0;							// we stored the cylinder first.
+	for(i=0,j=0; j< eggVerts.length; i++,j++) {
+		colorShapes[i] = eggVerts[j];
 		}
 	diaStart =i;
 	for(j=0; j< diaVerts.length; i++, j++) {
-		console.log(diaVerts[j])
 		colorShapes[i] = diaVerts[j];
 		}
 	// gndStart = i;						// next we'll store the ground-plane;
@@ -178,17 +177,17 @@ function initVertexBuffer(gl) {
 }
 
 function makeDiamond() {
-//==============================================================================
-// Make a diamond-like shape from two adjacent tetrahedra, aligned with Z axis.
+	//==============================================================================
+	// Make a diamond-like shape from two adjacent tetrahedra, aligned with Z axis.
 
-var ctrColr = new Float32Array([0.930, 0.605, 0.843]);	// pink
-var topColr = new Float32Array([0.628, 0.910, 0.854]);	// blue
-var botColr = new Float32Array([0.940, 0.913, 0.620]); //yellow
-var baseVerts = 6; // number of vertices for the base square
-diaVerts = new Float32Array(  ((baseVerts*8)) * floatsPerVertex);
-var topRadius = 0.8;
-var botRadius = 1;
-var wallHei = 0.2
+	var ctrColr = new Float32Array([0.930, 0.605, 0.843]);	// pink
+	var topColr = new Float32Array([0.628, 0.910, 0.854]);	// blue
+	var botColr = new Float32Array([0.940, 0.913, 0.620]); //yellow
+	var baseVerts = 6; // number of vertices for the base square
+	diaVerts = new Float32Array(  ((baseVerts*8)) * floatsPerVertex);
+	var topRadius = 0.8;
+	var botRadius = 1;
+	var wallHei = 0.2
 
 	for(v=1,j=0; v<2*baseVerts; v++,j+=floatsPerVertex) {	
 		// skip the first vertex--not needed.
@@ -293,100 +292,126 @@ var wallHei = 0.2
 	
 }
 
-function makeCylinder() {
-//==============================================================================
-// Make a cylinder shape from one TRIANGLE_STRIP drawing primitive, using the
-// 'stepped spiral' design described in notes.
-// Cylinder center at origin, encircles z axis, radius 1, top/bottom at z= +/-1.
-//
-	var ctrColr = new Float32Array([0.2, 0.2, 0.2]);	// dark gray
-	var topColr = new Float32Array([0.4, 0.7, 0.4]);	// light green
-	var botColr = new Float32Array([0.5, 0.5, 1.0]);	// light blue
-	var capVerts = 16;	// # of vertices around the topmost 'cap' of the shape
-	var botRadius = 1.6;		// radius of bottom of cylinder (top always 1.0)
-	
-	// Create a (global) array to hold this cylinder's vertices;
-	cylVerts = new Float32Array(  ((capVerts*6) -2) * floatsPerVertex);
-										// # of vertices * # of elements needed to store them. 
+function makeEgg() {
+	//==============================================================================
+	var ctrColr = new Float32Array([0.930, 0.605, 0.843]);	// pink
+	var topColr = new Float32Array([0.628, 0.910, 0.854]);	// blue
+	var botColr = new Float32Array([0.940, 0.913, 0.620]); //yellow
+	var slices = 7;		// # of slices of the sphere along the z axis. >=3 req'd
+	// (choose odd # or prime# to avoid accidental symmetry)
+	var sliceVerts	= 27;	// # of vertices around the top edge of the slice
 
-	// Create circle-shaped top cap of cylinder at z=+1.0, radius 1.0
-	// v counts vertices: j counts array elements (vertices * elements per vertex)
-	for(v=1,j=0; v<2*capVerts; v++,j+=floatsPerVertex) {	
-		// skip the first vertex--not needed.
-		if(v%2==0)
-		{				// put even# vertices at center of cylinder's top cap:
-			cylVerts[j  ] = 0.0; 			// x,y,z,w == 0,0,1,1
-			cylVerts[j+1] = 0.0;	
-			cylVerts[j+2] = 1.0; 
-			cylVerts[j+3] = 1.0;			// r,g,b = topColr[]
-			cylVerts[j+4]=ctrColr[0]; 
-			cylVerts[j+5]=ctrColr[1]; 
-			cylVerts[j+6]=ctrColr[2];
+	var sliceAngle = Math.PI/(slices * 2);	// lattitude angle spanned by one slice.
+
+	var topR = 2
+	var ctrR = 1
+	var botR = 0.8
+
+	// Create a (global) array to hold this sphere's vertices:
+	eggVerts = new Float32Array(  ((slices * 2* sliceVerts) -2 + sliceVerts * 2) * floatsPerVertex);
+
+	// INITIALIZE:
+	var cosBot = 0.0;					// cosine and sine of the lattitude angle for
+	var sinBot = 0.0;					// 	the current slice's BOTTOM (southward) edge. 
+	// (NOTE: Lattitude = 0 @equator; -90deg @south pole; +90deg at north pole)
+	var cosTop = 0.0;					// "	" " for current slice's TOP (northward) edge
+	var sinTop = 0.0;
+
+	var j = 0;							// initialize our array index
+	var isFirstSlice = 1;		// ==1 ONLY while making south-pole slice; 0 otherwise
+	var isLastSlice = 0;		// ==1 ONLY while making north-pole slice; 0 otherwise
+	for(s=0; s<slices; s++) {	// for each slice of the sphere,---------------------
+		// For current slice's top & bottom edges, find lattitude angle sin,cos:
+		if(s==0) {
+			isFirstSlice = 1;		// true ONLY when we're creating the south-pole slice
+			cosBot =  0.0; 			// initialize: first slice's lower edge is south pole.
+			sinBot = -1.0;			// (cos(lat) sets slice diameter; sin(lat) sets z )
 		}
-		else { 	// put odd# vertices around the top cap's outer edge;
-						// x,y,z,w == cos(theta),sin(theta), 1.0, 1.0
-						// 					theta = 2*PI*((v-1)/2)/capVerts = PI*(v-1)/capVerts
-			cylVerts[j  ] = Math.cos(Math.PI*(v-1)/capVerts);			// x
-			cylVerts[j+1] = Math.sin(Math.PI*(v-1)/capVerts);			// y
-			//	(Why not 2*PI? because 0 < =v < 2*capVerts, so we
-			//	 can simplify cos(2*PI * (v-1)/(2*capVerts))
-			cylVerts[j+2] = 1.0;	// z
-			cylVerts[j+3] = 1.0;	// w.
-			// r,g,b = topColr[]
-			cylVerts[j+4]=topColr[0]; 
-			cylVerts[j+5]=topColr[1]; 
-			cylVerts[j+6]=topColr[2];			
-		}
-	}
-	// Create the cylinder side walls, made of 2*capVerts vertices.
-	// v counts vertices within the wall; j continues to count array elements
-	for(v=0; v< 2*capVerts; v++, j+=floatsPerVertex) {
-		if(v%2==0)	// position all even# vertices along top cap:
-		{		
-				cylVerts[j  ] = Math.cos(Math.PI*(v)/capVerts);		// x
-				cylVerts[j+1] = Math.sin(Math.PI*(v)/capVerts);		// y
-				cylVerts[j+2] = 1.0;	// z
-				cylVerts[j+3] = 1.0;	// w.
-				// r,g,b = topColr[]
-				cylVerts[j+4]=topColr[0]; 
-				cylVerts[j+5]=topColr[1]; 
-				cylVerts[j+6]=topColr[2];			
-		}
-		else		// position all odd# vertices along the bottom cap:
-		{
-				cylVerts[j  ] = botRadius * Math.cos(Math.PI*(v-1)/capVerts);		// x
-				cylVerts[j+1] = botRadius * Math.sin(Math.PI*(v-1)/capVerts);		// y
-				cylVerts[j+2] =-1.0;	// z
-				cylVerts[j+3] = 1.0;	// w.
-				// r,g,b = topColr[]
-				cylVerts[j+4]=botColr[0]; 
-				cylVerts[j+5]=botColr[1]; 
-				cylVerts[j+6]=botColr[2];			
-		}
-	}
-	// Create the cylinder bottom cap, made of 2*capVerts -1 vertices.
-	// v counts the vertices in the cap; j continues to count array elements
-	for(v=0; v < (2*capVerts -1); v++, j+= floatsPerVertex) {
-		if(v%2==0) {	// position even #'d vertices around bot cap's outer edge
-			cylVerts[j  ] = botRadius * Math.cos(Math.PI*(v)/capVerts);		// x
-			cylVerts[j+1] = botRadius * Math.sin(Math.PI*(v)/capVerts);		// y
-			cylVerts[j+2] =-1.0;	// z
-			cylVerts[j+3] = 1.0;	// w.
-			// r,g,b = topColr[]
-			cylVerts[j+4]=botColr[0]; 
-			cylVerts[j+5]=botColr[1]; 
-			cylVerts[j+6]=botColr[2];		
-		}
-		else {				// position odd#'d vertices at center of the bottom cap:
-			cylVerts[j  ] = 0.0; 			// x,y,z,w == 0,0,-1,1
-			cylVerts[j+1] = 0.0;	
-			cylVerts[j+2] =-1.0; 
-			cylVerts[j+3] = 1.0;			// r,g,b = botColr[]
-			cylVerts[j+4]=botColr[0]; 
-			cylVerts[j+5]=botColr[1]; 
-			cylVerts[j+6]=botColr[2];
+		else {					// otherwise, set new bottom edge == old top edge
+			isFirstSlice = 0;	
+			cosBot = cosTop;
+			sinBot = sinTop;
+		}								// then compute sine,cosine of lattitude of new top edge.
+		cosTop = botR * Math.cos((-Math.PI/2) +(s+1)*sliceAngle); 
+		sinTop = Math.sin((-Math.PI/2) +(s+1)*sliceAngle);
+		for(v=isFirstSlice;    v< 2*sliceVerts-isLastSlice;   v++,j+=floatsPerVertex)
+		{						// for each vertex of this slice,
+			if(v%2 ==0) { // put vertices with even-numbered v at slice's bottom edge;
+										// by circling CCW along longitude (east-west) angle 'theta':
+										// (0 <= theta < 360deg, increases 'eastward' on sphere).
+										// x,y,z,w == cos(theta),sin(theta), 1.0, 1.0
+										// where			theta = 2*PI*(v/2)/capVerts = PI*v/capVerts
+				eggVerts[j  ] = topR * cosBot * Math.cos(Math.PI * v/sliceVerts);	// x
+				eggVerts[j+1] = topR * cosBot * Math.sin(Math.PI * v/sliceVerts);	// y
+				eggVerts[j+2] = sinBot;																			// z
+				eggVerts[j+3] = 1.0;																				// w.				
+			}
+			else {	// put vertices with odd-numbered v at the the slice's top edge
+							// (why PI and not 2*PI? because 0 <= v < 2*sliceVerts
+							// and thus we can simplify cos(2*PI* ((v-1)/2)*sliceVerts)
+							// (why (v-1)? because we want longitude angle 0 for vertex 1).  
+				eggVerts[j  ] = topR * cosTop * Math.cos(Math.PI * (v-1)/sliceVerts); 	// x
+				eggVerts[j+1] = topR * cosTop * Math.sin(Math.PI * (v-1)/sliceVerts);	// y
+				eggVerts[j+2] = sinTop;		// z
+				eggVerts[j+3] = 1.0;	
+			}
+			// finally, set some interesting colors for vertices:
+			if(v%3 == 0) { 	
+				eggVerts[j+4]=ctrColr[0]; 
+				eggVerts[j+5]=ctrColr[1]; 
+				eggVerts[j+6]=ctrColr[2];				
+				}
+			else if(v%3 == 1) {	
+				eggVerts[j+4]=botColr[0]; 
+				eggVerts[j+5]=botColr[1]; 
+				eggVerts[j+6]=botColr[2];	
+				}
+			else if(v%3 == 2) {
+				eggVerts[j+4]=topColr[0]; 
+				eggVerts[j+5]=topColr[1]; 
+				eggVerts[j+6]=topColr[2];	
+			}
+
 		}
 	}
+
+	cosBot = cosTop;
+	sinBot = sinTop;
+	for(v=0;    v< 2*sliceVerts;   v++,j+=floatsPerVertex)
+	{						
+		if(v%2 ==0) { 
+
+			eggVerts[j  ] = topR * cosBot * Math.cos(Math.PI * v/sliceVerts);	// x
+			eggVerts[j+1] = topR * cosBot * Math.sin(Math.PI * v/sliceVerts);	// y
+			eggVerts[j+2] = sinBot;																			// z
+			eggVerts[j+3] = 1.0;																				// w.				
+		}
+		else {
+ 
+			eggVerts[j  ] = 0
+			eggVerts[j+1] = 0
+			eggVerts[j+2] = sinTop;		// z
+			eggVerts[j+3] = 1.0;	
+		}
+		// finally, set some interesting colors for vertices:
+		if(v%3 == 0) { 	
+			eggVerts[j+4]=ctrColr[0]; 
+			eggVerts[j+5]=ctrColr[1]; 
+			eggVerts[j+6]=ctrColr[2];				
+			}
+		else if(v%3 == 1) {	
+			eggVerts[j+4]=botColr[0]; 
+			eggVerts[j+5]=botColr[1]; 
+			eggVerts[j+6]=botColr[2];	
+			}
+		else if(v%3 == 2) {
+			eggVerts[j+4]=topColr[0]; 
+			eggVerts[j+5]=topColr[1]; 
+			eggVerts[j+6]=topColr[2];	
+		}
+	}
+
+	
 }
 
 function makeGroundGrid() {
@@ -465,8 +490,8 @@ function draw(gl, n, currentAngle, modelMatrix, u_ModelMatrix) {
   gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
   // Draw the cylinder's vertices, and no other vertices:
   gl.drawArrays(gl.TRIANGLE_STRIP,				// use this drawing primitive, and
-  							cylStart/floatsPerVertex, // start at this vertex number, and
-  							cylVerts.length/floatsPerVertex);	// draw this many vertices.
+  							eggSatrt/floatsPerVertex, // start at this vertex number, and
+  							eggVerts.length/floatsPerVertex);	// draw this many vertices.
 
  //-------Draw Spinning Diamonds:
  modelMatrix.setTranslate( 0.4, 0.4, 0.0); // 'set' means DISCARD old matrix,
